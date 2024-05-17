f55ae4cb6821a4e729a860b89295d8678c13a853
felixogon <felixmichael.ogon@gmail.com>
2024-05-17 20:11:21 +0200

    Update algo_strategy.py
    More turrets on side

--- python-algo/algo_strategy.py
+++ python-algo/algo_strategy.py
@@ -56,7 +56,7 @@ class AlgoStrategy(gamelib.AlgoCore):
         game_state = gamelib.GameState(self.config, turn_state)
 
         gamelib.debug_write('Performing turn {} of your custom algo strategy'.format(game_state.turn_number))
-        game_state.suppress_warnings(True)  #Comment or remove this line to enable warnings.
+        game_state.suppress_warnings(True)  # Comment or remove this line to enable warnings.
         self.enemy_health_check(game_state)
         self.start_strategy_1(game_state)
         game_state.submit_turn()
@@ -92,8 +92,15 @@ class AlgoStrategy(gamelib.AlgoCore):
         
         third_layer_support = [[12, 7], [15, 7], [12, 6], [15, 6], [12, 5], [15, 5], [12, 4], [15, 4]]
         
-        if game_state.turn_number <= 100 :
+        # Additional turrets on the left and right edges
+        side_turrets = []
+        for y in range(14, -1, -1):  # Fill from top to bottom
+            side_turrets.append([0, y])
+            side_turrets.append([27, y])
+        
+        if game_state.turn_number <= 100:
             game_state.attempt_spawn(TURRET, first_layer_turrets_a)
+            game_state.attempt_spawn(TURRET, side_turrets)  # Add the side turrets
             game_state.attempt_upgrade(second_layer_support)
             game_state.attempt_upgrade(first_layer_support)
             game_state.attempt_spawn(SUPPORT, first_layer_support)
@@ -165,50 +172,4 @@ class AlgoStrategy(gamelib.AlgoCore):
             damage = 0
             for path_location in path:
                 # Get number of enemy turrets that can attack each location and multiply by turret damage
-                damage += len(game_state.get_attackers(path_location, 0)) * gamelib.GameUnit(TURRET, game_state.config).damage_i
-            damages.append(damage)
-        
-        # Now just return the location that takes the least damage
-        return location_options[damages.index(min(damages))]
-
-    def detect_enemy_unit(self, game_state, unit_type=None, valid_x = None, valid_y = None):
-        total_units = 0
-        for location in game_state.game_map:
-            if game_state.contains_stationary_unit(location):
-                for unit in game_state.game_map[location]:
-                    if unit.player_index == 1 and (unit_type is None or unit.unit_type == unit_type) and (valid_x is None or location[0] in valid_x) and (valid_y is None or location[1] in valid_y):
-                        total_units += 1
-        return total_units
-        
-    def filter_blocked_locations(self, locations, game_state):
-        filtered = []
-        for location in locations:
-            if not game_state.contains_stationary_unit(location):
-                filtered.append(location)
-        return filtered
-
-    def on_action_frame(self, turn_string):
-        """
-        This is the action frame of the game. This function could be called 
-        hundreds of times per turn and could slow the algo down so avoid putting slow code here.
-        Processing the action frames is complicated so we only suggest it if you have time and experience.
-        Full doc on format of a game frame at in json-docs.html in the root of the Starterkit.
-        """
-        # Let's record at what position we get scored on
-        state = json.loads(turn_string)
-        events = state["events"]
-        breaches = events["breach"]
-        for breach in breaches:
-            location = breach[0]
-            unit_owner_self = True if breach[4] == 1 else False
-            # When parsing the frame data directly, 
-            # 1 is integer for yourself, 2 is opponent (StarterKit code uses 0, 1 as player_index instead)
-            if not unit_owner_self:
-                gamelib.debug_write("Got scored on at: {}".format(location))
-                self.scored_on_locations.append(location)
-                gamelib.debug_write("All locations: {}".format(self.scored_on_locations))
-
-
-if __name__ == "__main__":
-    algo = AlgoStrategy()
-    algo.start()
+                damage += len(game_state.get_attackers(path_location, 0)) * gamelib
