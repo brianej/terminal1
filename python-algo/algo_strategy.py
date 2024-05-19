8f88c110e38e8c00a87b5d5280ac796a16144939
felixogon <felixmichael.ogon@gmail.com>
2024-05-19 15:55:06 +0200

    Update algo_strategy.py

--- python-algo/algo_strategy.py
+++ python-algo/algo_strategy.py
@@ -43,7 +43,7 @@ class AlgoStrategy(gamelib.AlgoCore):
         game_state = gamelib.GameState(self.config, turn_state)
 
         gamelib.debug_write('Performing turn {} of your custom algo strategy'.format(game_state.turn_number))
-        game_state.suppress_warnings(True)  
+        game_state.suppress_warnings(True)  # Comment or remove this line to enable warnings.
         
         self.start_strategy_1(game_state)
         
@@ -70,16 +70,13 @@ class AlgoStrategy(gamelib.AlgoCore):
             self.before_10(game_state)
             
         if game_state.turn_number % 4 == 0 and game_state.turn_number > 10:
-            if random.randint(1, 4) in [1, 2, 3]:
-                if game_state.turn_number >= 20:
-                    game_state.attempt_spawn(DEMOLISHER, [12, 1], 1000)
-                else:
-                    game_state.attempt_spawn(SCOUT, [12, 1], 1000)
+            if game_state.turn_number >= 20:
+                game_state.attempt_spawn(DEMOLISHER, [17, 3], 1000)
             else:
-                if game_state.turn_number >= 20:
-                    game_state.attempt_spawn(DEMOLISHER, [12, 1], 1000)
+                if random.randint(1, 4) in [1, 2, 3]:
+                    game_state.attempt_spawn(SCOUT, [17, 3], 1000)
                 else:
-                    game_state.attempt_spawn(SCOUT, [12, 1], 1000)
+                    game_state.attempt_spawn(SCOUT, [17, 3], 1000)
 
         if game_state.turn_number in [10, 16, 21, 25]:
             self.send_interceptors(game_state, [26, 13])
@@ -103,7 +100,7 @@ class AlgoStrategy(gamelib.AlgoCore):
             [18, 8], [18, 9], [18, 11]
         ]
         
-        # Build turrets and walls
+        # Build turrets and walls as per the previous strategy
         first_layer_turrets_a = [[1, 13], [2, 13], [3, 13], [22, 13], [23, 13], [24, 13], [27, 13], [2, 12], [5, 12], 
                                  [6, 12], [9, 12], [10, 12], [13, 12], [14, 12], [17, 12], [18, 12], [20, 12], 
                                  [21, 12], [26, 12], [5, 11], [9, 11], [13, 11], [17, 11], [20, 11]]
@@ -122,9 +119,20 @@ class AlgoStrategy(gamelib.AlgoCore):
         if game_state.turn_number > 7:
             game_state.attempt_spawn(TURRET, second_layer_turrets)
             game_state.attempt_spawn(WALL, first_layer_walls)
-            
+
+        # Build and upgrade more supports earlier
+        more_support_locations = [[18, 6], [17, 6], [16, 6], [15, 6], [14, 6], [13, 6]]
+        game_state.attempt_spawn(SUPPORT, more_support_locations)
+        game_state.attempt_upgrade(more_support_locations)
+
         game_state.attempt_upgrade(support_locations)
         game_state.attempt_spawn(SUPPORT, support_locations)
+
+        # Upgrade the four turrets on the very right
+        right_turrets = [[24, 13], [27, 13], [26, 12], [25, 11]]
+        game_state.attempt_upgrade(right_turrets)
+
+        # Do not upgrade other turrets unless specified
         game_state.attempt_upgrade(first_layer_walls)
     
     def before_10(self, game_state):
@@ -136,16 +144,14 @@ class AlgoStrategy(gamelib.AlgoCore):
         
         all_spawn = [[11, 2], [16, 2], [12, 1], [15, 1], [13, 0], [14, 0], [0, 13], [27, 13], 
                      [1, 12], [26, 12], [2, 11], [25, 11], [3, 10], [24, 10], [4, 9], [23, 9], 
-                     [5, 8], [22, 8], [6, 7], [21, 7], [7, 6], [20, 6], [8, 5], [19, 5], [9, 4], 
-                     [18, 4], [10, 3], [17, 3]]
-        
+                     [5, 8], [22, 8], [6, 7], [21, 7], [7, 6], [20, 6], [8, 5], [19, 5],
+                     [9, 4], [18, 4], [10, 3], [17, 3]]
         best_location = self.least_damage_spawn_location(game_state, spawn_location)
         
         if game_state.turn_number == 3:
             game_state.attempt_spawn(SCOUT, best_location, 13)
         elif game_state.turn_number == 8:
             game_state.attempt_spawn(SCOUT, best_location, 15)
-        
         
     def early_support(self, game_state):
         global RUSH
@@ -209,15 +215,17 @@ class AlgoStrategy(gamelib.AlgoCore):
         # Now just return the location that takes the least damage
         return location_options[damages.index(min(damages))]
 
-    def detect_enemy_unit(self, game_state, unit_type=None, valid_x = None, valid_y = None):
+    def detect_enemy_unit(self, game_state, unit_type=None, valid_x=None, valid_y=None):
         total_units = 0
         for location in game_state.game_map:
             if game_state.contains_stationary_unit(location):
                 for unit in game_state.game_map[location]:
-                    if unit.player_index == 1 and (unit_type is None or unit.unit_type == unit_type) and (valid_x is None or location[0] in valid_x) and (valid_y is None or location[1] in valid_y):
+                    if unit.player_index == 1 and (unit_type is None or unit.unit_type == unit_type) and (
+                            valid_x is None or location[0] in valid_x) and (
+                            valid_y is None or location[1] in valid_y):
                         total_units += 1
         return total_units
-        
+
     def filter_blocked_locations(self, locations, game_state):
         filtered = []
         for location in locations:
@@ -232,7 +240,7 @@ class AlgoStrategy(gamelib.AlgoCore):
         Processing the action frames is complicated so we only suggest it if you have time and experience.
         Full doc on format of a game frame at in json-docs.html in the root of the Starterkit.
         """
-        
+        # Let's record at what position we get scored on
         state = json.loads(turn_string)
         events = state["events"]
         breaches = events["breach"]
@@ -242,9 +250,8 @@ class AlgoStrategy(gamelib.AlgoCore):
             # When parsing the frame data directly, 
             # 1 is integer for yourself, 2 is opponent (StarterKit code uses 0, 1 as player_index instead)
             if not unit_owner_self:
-                gamelib.debug_write("Got scored on at: {}".format(location))
+                 gamelib.debug_write("Got scored on at: {}".format(location))
                 self.scored_on_locations.append(location)
-                gamelib.debug_write("All locations: {}".format(self.scored_on_locations))
 
 
 if __name__ == "__main__":
